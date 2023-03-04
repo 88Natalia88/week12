eda8423633361197d0d1fa2225b7703b3259892f
Natalia Balashova <88karamelka88@mail.ru>
2023-03-04 22:05:58 +0300

    date

--- js/index.js
+++ js/index.js
@@ -63,7 +63,39 @@ document.querySelector('#img').src = imgs[Math.floor(Math.random()*imgs.length)]
 }
 
 //задание под звездочкой week 13
+const days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда',
+ 'Четверг', 'Пятница', 'Суббота'];
 
+function formatDate(date = new Date) {
+    let Y = date.getFullYear();
+    let M = date.getMonth() + 1;
+    let D = date.getDate();
+    let d = days[date.getDay()];
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let diffMs = new Date() - date;
+    let diffSec = Math.round(diffMs / 1000);
+    let diffMin = diffSec / 60;
+    let diffHour = diffMin / 60;
+  
+    // форматирование
+    Y = Y.toString().slice(-2);
+    M = M < 10 ? '0' + M : M;
+    D = D < 10 ? '0' + D : D;
+    h = h < 10 ? '0' + h : h;
+    m = m < 10 ? '0' + m : m;
+  
+    if (diffSec < 1) {
+      return 'прямо сейчас';
+    } else if (diffMin < 1) {
+      return `${diffSec} сек. назад`
+    } else if (diffHour < 1) {
+      return `${diffMin} мин. назад`
+    } else {
+      return `${D}.${M}.${Y} ${h}:${m} (${d})`
+    }
+  }
+ console.log(formatDate());
 
 //задание под звездочкой week 12
 let str = 'Hello!<br>Welcome to the study JavaScript!<hr><p>Some more content here</p><img src="">';
